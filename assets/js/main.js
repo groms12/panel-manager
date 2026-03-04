340dbf9bc0001f3e65335a1c607abacd5e162fd9
DPopov14 <DPopov14@slb.com>
2026-03-05 01:00:49 +0300

    change png to svg, add settings to js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,17 @@
 $('.menu__open').click(function() {
     $('.menu__item + ul').removeClass('list--active');
-    $('.menu__item').removeClass('menu__item--active');
+    // $('.menu__item').removeClass('menu__item--active');
     $('.menu__icon').toggleClass('menu__icon--active');
     $('.menu').toggleClass('menu--active')
 });
 
 $('.menu__item').click(function() {
+    if (! $('.menu').hasClass('menu--active')){
+        $('.menu').toggleClass('menu--active');
+        $('.menu__icon').toggleClass('menu__icon--active');
+    }
+    $('.menu__item').removeClass('menu__item--active');
+    
     $(this).addClass('menu__item--active');
     $(this).next().addClass('list--active');
 })
