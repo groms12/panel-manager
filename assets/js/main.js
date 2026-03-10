c131d8799e06b82cd68b9a5095e7cde7a753df48
DPopov14 <DPopov14@slb.com>
2026-03-10 23:57:37 +0300

    add cities - block

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,12 +6,17 @@ $('.menu__open').click(function() {
 });
 
 $('.menu__item').click(function() {
-    if (! $('.menu').hasClass('menu--active')){
+    if (!$('.menu').hasClass('menu--active')) {
         $('.menu').toggleClass('menu--active');
         $('.menu__icon').toggleClass('menu__icon--active');
     }
     $('.menu__item').removeClass('menu__item--active');
-    
+
     $(this).addClass('menu__item--active');
     $(this).next().addClass('list--active');
-})
+});
+
+$('.city').click(function() {
+    $(this).children('.city__offices').toggleClass('city__offices--active');
+    $(this).children('.city__arrow').toggleClass('city__arrow--active');
+});
